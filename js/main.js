28fc7f1632dd6934df281830c8c7515dd57b15f1
bradd evans <braddevans123@gmail.com>
2021-03-08 20:22:04 +0000

    start placing functions and values into classes specific to their uses

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
-$(document).ready(function() {
-    $("#generaterandpass").click(function(event) {
-        console.log("Owo: " + Math.random().toString(36).substring(2, 36) + Math.random().toString(36).substring(2, 36));
+class passClass {
+    temppassword = {password: "", options: [], generatedAt: 0, url: ""};
+
+
+    getRandPass = function (maxlength, special, alphanumeric) {
+        let pass = "";
+        pass = Math.random().toString(36).substring(2, 36) + Math.random().toString(36).substring(2, 36);
+
+        maxlength != null ? pass.substr(0, maxlength) : pass;
+
+        return pass;
+    }
+}
+
+
+/*
+ * Document Listeners
+ */
+$(document).ready(function () {
+    $("#generaterandpass").click(function (event) {
+        console.log("Owo: " + new passClass().getRandPass());
     });
 });
